0d1fe53a9420c95601b27746e83a1df716f33ea3
zhuxiaoliang <836904717@qq.com>
2018-07-05 15:29:20 +0800

    使用python反射机制实现通过url动态路由

--- people/client_ip.py
+++ people/client_ip.py
@@ -1,9 +1,20 @@
 #author_by zhuxiaoliang
 #2018-07-04 下午10:42
 from django.http import HttpResponse
+from django.shortcuts import reverse,redirect
 import time
+#python反射机制
+def process(request,**kwargs):
 
-def resolv_client(request):
-	return HttpResponse('pig.')
-
-#mysite.cheungssh.cheungssh.docker_images_list
+    app = kwargs.get('app','people')
+    function = kwargs.get('function','UserView')
+    try:
+        appobj = __import__("{}.views".format(app))
+        viewobj = getattr(appobj,'views')
+        funcobj = getattr(viewobj,function)
+        ret = funcobj()
+    except (ImportError,AttributeError) as e:
+        return HttpResponse("404 Not Found")
+    except Exception as e:
+        return redirect(reverse('users'))
+    return HttpResponse(ret)
--- people/views.py
+++ people/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,reverse
 from django.http import HttpResponse
+from  django.shortcuts import redirect,HttpResponse,resolve_url
+
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.versioning import QueryParameterVersioning
@@ -24,9 +26,17 @@ class resolv_client(APIView):
         }
         return HttpResponse(json.dumps(info))
 
+
+
+
+
 class UserView(APIView):
     versioning_class = URLPathVersioning
     def get(self,request,*args,**kwargs):
         print(request.versioning_scheme.reverse(viewname='users',request=request))
         #request.version
         return HttpResponse(request.version)
+
+
+def runpy():
+    return ('hi,pig.')
